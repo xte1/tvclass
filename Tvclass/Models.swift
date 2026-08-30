b850389f8045b8a1e18d99b69944bf9b71be2ec3
dev <anwar.nille@gmail.com>
2026-08-31 02:47:47 +0300

    Update Models.swift

--- Tvclass/Models.swift
+++ Tvclass/Models.swift
@@ -17,9 +17,15 @@ struct MediaItem: Identifiable, Codable {
     }
 
     var displayTitle: String { title ?? name ?? "بدون عنوان" }
+    
     var posterURL: URL? {
         guard let path = posterPath else { return nil }
         return URL(string: "https://image.tmdb.org/t/p/w500\(path)")
+    }
+    
+    var backdropURL: URL? {
+        guard let path = backdropPath else { return nil }
+        return URL(string: "https://image.tmdb.org/t/p/w780\(path)")
     }
 }
 
