d2f3fc3b088bdb9843d0a09cfb8978ba7c8acaf0
dev <anwar.nille@gmail.com>
2026-08-31 03:07:17 +0300

    Update Models.swift

--- Tvclass/Models.swift
+++ Tvclass/Models.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct MediaItem: Identifiable, Codable {
+struct MediaItem: Identifiable, Codable, Equatable {
     let id: Int
     let title: String?
     let name: String?
@@ -8,34 +8,29 @@ struct MediaItem: Identifiable, Codable {
     let posterPath: String?
     let backdropPath: String?
     let voteAverage: Double?
+    let mediaType: String?
 
     enum CodingKeys: String, CodingKey {
         case id, title, name, overview
         case posterPath = "poster_path"
         case backdropPath = "backdrop_path"
         case voteAverage = "vote_average"
+        case mediaType = "media_type"
     }
 
     var displayTitle: String { title ?? name ?? "بدون عنوان" }
-    
+
     var posterURL: URL? {
         guard let path = posterPath else { return nil }
         return URL(string: "https://image.tmdb.org/t/p/w500\(path)")
     }
-    
+
     var backdropURL: URL? {
         guard let path = backdropPath else { return nil }
-        return URL(string: "https://image.tmdb.org/t/p/w780\(path)")
+        return URL(string: "https://image.tmdb.org/t/p/w1280\(path)")
     }
 }
 
 struct MediaResponse: Codable {
     let results: [MediaItem]
-}
-
-struct Episode: Identifiable {
-    let id: Int
-    let title: String
-    let episodeNumber: Int
-    let videoURL: URL
 }
